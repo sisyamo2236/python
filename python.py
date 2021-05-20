c114faa64e782944e4f1501ad6a022f8ce31a6ee
sisyamo2236@gmail.com <sisyamo2236@gmail.com>
2021-05-20 21:17:22 +0900

    追加.py

--- python.py
+++ python.py
@@ -1,4 +1,19 @@
 
 
-for i in range(1000)
+#pythonにはインデントがないので気を付けて
+for i in range(10):
     print(i)
+    print( )
+    print(i * 2)
+#Pythonには変数の型を宣言する必要ない
+
+a=("a")
+print(a)
+a=(1000.515656)
+print(a)
+
+a = 2
+if a >= 10:
+    print("OK")
+else:
+    print("NG")
---
+++ q_1.py
@@ -0,0 +1,6 @@
+
+b = 2
+if b >= 0:
+    print("77")
+else:
+    print("7")
