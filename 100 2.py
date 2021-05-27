6edb63a7139bf5ef1408abcca8824e754bc1ce9e
sisyamo2236@gmail.com <sisyamo2236@gmail.com>
2021-05-27 19:46:56 +0900

    追加

---
+++ 100 2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#
+
+#---------------------------------------- checkPrime
+#
+# 素数チェック関数
+#  戻り値
+#    True  : 素数
+#    False : 素数ではない
+#
+def checkPrime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+#---------------------------------------- main
+# num : 素数候補
+
+for num in range(2, 101):
+    if checkPrime(num):
+        print(' ' + str(num), end='')
+
+print()
